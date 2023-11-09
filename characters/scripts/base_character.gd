5e394ee4fa44610d9421bc801262b4149b33f652
davi bandeira <fcodavibandeira@edu.unifor.br>
2023-11-09 09:21:43 -0300

    character vertical movement and animations

--- characters/scripts/base_character.gd
+++ characters/scripts/base_character.gd
@@ -1,22 +1,32 @@
 extends CharacterBody3D
 class_name BaseCharacter
 
+var _jump_count: int = 0
 var _current_speed: float
 
 @export_category("Variables")
-@export var _walk_speed: float = 2.0
+@export var _gravity: float = 9.8
 @export var _run_speed: float = 3.5
+@export var _walk_speed: float = 2.0
+@export var _jump_speed: float = 7.0
 
 @export_category("Objects")
 @export var _body: CharacterBody
 @export var _spring_arm_offset: CharacterArm
 
 func _physics_process(_delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= _gravity * _delta
+		
+	_jump()
 	_move()
 	move_and_slide()
 	_body.animate(velocity)
 	
-	
+	if is_on_floor():
+		_jump_count = 0
+		
+		
 func _move() -> void:
 	var _input_direction: Vector2 = (
 		Input.get_vector(
@@ -49,3 +59,12 @@ func is_running() -> bool:
 		
 	_current_speed = _walk_speed
 	return false
+	
+	
+func _jump() -> void:
+	if Input.is_action_just_pressed("ui_accept") and _jump_count < 2:
+		velocity.y = _jump_speed
+		_jump_count += 1
+		
+		if _jump_count > 1:
+			_body.animate_action("Flip")
--- characters/scripts/character_body.gd
+++ characters/scripts/character_body.gd
@@ -3,6 +3,8 @@ class_name CharacterBody
 
 const _LERP_VELOCITY: float = 0.15
 
+var _on_action: bool = false
+
 @export_category("Objects")
 @export var _character: BaseCharacter
 @export var _animation: AnimationPlayer
@@ -16,12 +18,42 @@ func apply_rotation(_velocity: Vector3) -> void:
 	
 	
 func animate(_velocity: Vector3) -> void:
-	if _velocity:
-		if _character.is_running():
-			_animation.play("Run")
-			return
-			
-		_animation.play("Walk")
+	if _on_action:
+		return
+		
+	if _velocity.y != 0:
+		_vertical_animations(_velocity)
+		return
+		
+	if _velocity.x != 0:
+		_horizontal_animations(_velocity)
 		return
 		
 	_animation.play("Idle")
+	
+	
+func _horizontal_animations(_velocity: Vector3) -> void:
+	if _character.is_running():
+		_animation.play("Run")
+		return
+		
+	_animation.play("Walk")
+	
+	
+func _vertical_animations(_velocity: Vector3) -> void:
+	if _velocity.y > 0:
+		_animation.play("Jump")
+		
+	if _velocity.y < 0:
+		_animation.play("Fall")
+		
+		
+func animate_action(_action: String) -> void:
+	_animation.play(_action)
+	_on_action = true
+	
+	
+func _on_animation_finished(_anim_name: String) -> void:
+	match _anim_name:
+		"Flip":
+			_on_action = false
