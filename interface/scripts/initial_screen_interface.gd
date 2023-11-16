b471360da9cb61589c3ef4697926372dd7905907
davi bandeira <fcodavibandeira@edu.unifor.br>
2023-11-16 08:10:02 -0300

    how to play screen

--- interface/scripts/initial_screen_interface.gd
+++ interface/scripts/initial_screen_interface.gd
@@ -7,6 +7,7 @@ var _can_interact: bool = false
 @export_category("Objects")
 @export var _character_name: Label
 @export var _character_stats: VBoxContainer
+@export var _how_to_play_container: ColorRect
 
 func _ready() -> void:
 	get_tree().paused = false
@@ -22,7 +23,7 @@ func _mouse_interaction(_button, _state: String) -> void:
 		"exited":
 			_button_in_range = ""
 			_can_interact = false
-			_button.modulate.a = .8
+			_button.modulate.a = 1.0
 			if _button.name == "Sophia" or _button.name == "Gobot":
 				_display_character_data("")
 				_character_name.text = ""
@@ -30,17 +31,23 @@ func _mouse_interaction(_button, _state: String) -> void:
 		"entered":
 			_can_interact = true
 			_button_in_range = _button.name
-			_button.modulate.a = .5
+			_button.modulate.a = .6
 			if _button.name == "Sophia" or _button.name == "Gobot":
 				_display_character_data(_button.name)
 				_character_name.text = _button.name
 				
 				
 func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("esc") and _how_to_play_container.visible:
+		_how_to_play_container.hide()
+		
 	if _can_interact and Input.is_action_just_pressed("left_click"):
 		match _button_in_range:
 			"Quit":
 				get_tree().quit()
+				
+			"HowToPlay":
+				_how_to_play_container.show()
 				
 		if _button_in_range == "Sophia" or _button_in_range == "Gobot":
 			game_data.character_scene_path = game_data.characters_data[_button_in_range]["scene_path"]
